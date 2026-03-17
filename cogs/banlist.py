8a392af8b60617e7d0f5ead5eac9d4fde311b08a
Bombface68 <bombface68@gmail.com>
2026-03-16 19:59:20 -0700

    Update banlist.py

--- cogs/banlist.py
+++ cogs/banlist.py
@@ -21,7 +21,7 @@ class Banlist(commands.Cog):
         banlist_channel = self.bot.get_channel(config.BAN_LOG_CHANNEL_ID)
         embed = discord.Embed(
             title="User Banned",
-            description=f"{audit_log.target.mention} has been banned from the server.\nReason: {audit_log.reason}\nBanned by: {audit_log.user.mention}",
+            description=f"{audit_log.target.mention} - {audit_log.target.display_name} has been banned from the server.\nReason: {audit_log.reason}\nBanned by: {audit_log.user.mention}",
             color=discord.Color.red()
         )
         embed.set_thumbnail(url=audit_log.target.display_avatar.url)
@@ -41,49 +41,12 @@ class Banlist(commands.Cog):
         banlist_channel = self.bot.get_channel(config.BAN_LOG_CHANNEL_ID)
         embed = discord.Embed(
             title="User Unbanned",
-            description=f"{audit_log.target.mention} has been unbanned from the server.\nUnbanned by: {audit_log.user.mention}",
+            description=f"{audit_log.target.mention} - {audit_log.target.display_name} has been unbanned from the server.\nUnbanned by: {audit_log.user.mention}",
             color=discord.Color.green()
         )
         embed.set_thumbnail(url=audit_log.target.display_avatar.url)
         await banlist_channel.send(embed=embed)
         await log(self.bot, f'LOG: User {audit_log.target.mention} has been unbanned from the server')
             
-    @app_commands.command(name='minecraft_ban', description='Report a banned user from the Minecraft server!')
-    async def minecraft_ban(self, interaction: discord.Interaction, username: str, reason: str = "No reason provided"):
-        if config.ADMIN_ROLE_ID not in [role.id for role in interaction.user.roles] and config.MOD_ROLE_ID not in [role.id for role in interaction.user.roles]:
-            await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
-            await log(self.bot, f'ERROR: User {interaction.user.mention} attempted to use `minecraft_ban` without permission')
-            return
-        
-        # Minecraft server ban 
-        
-        await interaction.response.send_message(f'User {username} has been banned from the Minecraft server for: {reason}', ephemeral=True)
-        await log(self.bot, f'LOG: Minecraft user {username} has been banned by {interaction.user.mention} for: {reason}')
-        banlist_channel = self.bot.get_channel(config.BAN_LOG_CHANNEL_ID)
-        embed = discord.Embed(
-            title="Minecraft User Banned",
-            description=f"User {username} has been banned from the Minecraft server.\nReason: {reason}\nBanned by: {interaction.user.mention}",
-            color=discord.Color.red()
-        )
-        await banlist_channel.send(embed=embed)
-        
-    @app_commands.command(name='minecraft_unban', description='Report an unbanned user from the Minecraft server!')
-    async def minecraft_unban(self, interaction: discord.Interaction, username: str):
-        if config.ADMIN_ROLE_ID not in [role.id for role in interaction.user.roles] and config.MOD_ROLE_ID not in [role.id for role in interaction.user.roles]:
-            await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
-            await log(self.bot, f'ERROR: User {interaction.user.mention} attempted to use `minecraft_unban` without permission')
-            return
-
-        # Minecraft server unban
-
-        await interaction.response.send_message(f'User {username} has been unbanned from the Minecraft server.', ephemeral=True)
-        await log(self.bot, f'LOG: Minecraft user {username} has been unbanned by {interaction.user.mention}')
-        banlist_channel = self.bot.get_channel(config.BAN_LOG_CHANNEL_ID)
-        embed = discord.Embed(
-            title="Minecraft User Unbanned",
-            description=f"User {username} has been unbanned from the Minecraft server.\nUnbanned by: {interaction.user.mention}",
-            color=discord.Color.green()
-        )
-        await banlist_channel.send(embed=embed)
 async def setup(bot):
     await bot.add_cog(Banlist(bot))
